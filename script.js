3624feea7f607c63c2d9b9ca7a4e05c4a680431c
José Adrián Flores <140577003+insightvigil@users.noreply.github.com>
2025-06-15 14:56:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var css = document.querySelector("h3")
+var color1 = document.querySelector(".color1")
+var color2 = document.querySelector(".color2")
+var body = document.getElementById("gradient")
+
+
+
+/*
+FULL FORM 
+
+color1.addEventListener("input", function(){
+    body.style.background = "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+
+})
+
+console.log(color1.value);
+
+color2.addEventListener("input", function(){
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+})
+
+*/
+
+//Compact Form 
+function setGradient() { 
+    body.style.background = "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input",setGradient)
+color2.addEventListener("input",setGradient)
